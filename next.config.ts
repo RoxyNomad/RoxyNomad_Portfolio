a0217aaff27e0a53b6687117611349463088fc3c
Roxana Zwicky <roxana_zwicky@roxynomad.com>
2025-04-29 13:47:08 +0200

    fix: next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,18 +1,10 @@
 // next.config.ts
-import path from 'path';
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   pageExtensions: ['tsx', 'ts', 'jsx', 'js'],
-  webpack(config) {
-    config.resolve = config.resolve || {};
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      '@': path.resolve(__dirname, 'src'),
-    };
-    return config;
-  },
+  
 };
 
 export default nextConfig;
