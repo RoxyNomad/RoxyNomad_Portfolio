a332ec36ed036b8d0a78eff9191e3ad0172ffe27
Roxana Zwicky <roxana_zwicky@roxynomad.com>
2025-06-10 16:56:33 +0200

    feat: add <br />

--- src/adapters/ui/components/AboutMe/AboutMe.tsx
+++ src/adapters/ui/components/AboutMe/AboutMe.tsx
@@ -21,22 +21,24 @@ const AboutMe = () => {
             Hey, ich bin Roxana – Ich bringe Ihre Videos und Ideen zum Leben!
           </p>
           <p className={styles.aboutMeText}>
-            Als leidenschaftlicher Video Editor und Cutter habe ich ein feines Gespür für Timing, Rhythmus und Emotionen. 
+            Als leidenschaftlicher Video Editor und Cutter habe ich ein feines Gespür für Timing, Rhythmus und Emotionen. <br />
             Mit diesen Fähigkeiten verwandle ich Ihr Rohmaterial in fesselnde Geschichten.
           </p>
           <p className={styles.aboutMeText}>
             Brauchen Sie ein stimmungsvolles Video Ihrer letzten Reise, 
-            ein oder mherere YouTube Videos oder professionelle Social-Media-Beiträge für Ihr Instagram oder TikTok? 
+            ein oder mherere YouTube Videos oder professionelle Social-Media-Beiträge für Ihr Instagram oder TikTok? <br />
             Ich liefere Ihnen präzise geschnittene Inhalte mit Stil und Struktur.
           </p>
           <p className={styles.aboutMeSubtitle}>
             Was mich auszeichnet:
           </p>
           <p className={styles.aboutMeText}>
-            Gefühl für Storytelling & Dramaturgie
-            Schneller, intuitiver Workflow mit DaVinci Resolve
-            Ausgeprägte Kreativität durch künstlerisches Training
-            Liebe zum Detail und hohe Qualitätsansprüche
+            Gefühl für Storytelling & Dramaturgie <br />
+            Präzision und Effizienz im Videoschnitt <br />
+            Kreative Ideen für visuelle Effekte und Übergänge <br />
+            Schneller, intuitiver Workflow mit DaVinci Resolve <br />
+            Ausgeprägte Kreativität durch künstlerisches Training <br />
+            Liebe zum Detail und hohe Qualitätsansprüche <br />
           </p>
           <p className={styles.aboutMeSubtitle}>
             Bereit für dein nächstes Projekt?
